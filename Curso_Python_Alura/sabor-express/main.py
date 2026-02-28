6e83e886933179c2ed9d74df368b32bf85460978
Thiago Nunes <thiagonesi.cntt@hotmail.com>
2026-02-28 11:18:28 -0300

    Teste main e utilização de boas praticas com match case

--- Curso_Python_Alura/sabor-express/main.py
+++ Curso_Python_Alura/sabor-express/main.py
@@ -1,24 +1,35 @@
 import os
 
-print("""🅂🄰🄱🄾🅁 🄴🅇🄿🅁🄴🅂🅂""")
+def exibir_nome_do_programa():
+    print("""🅂🄰🄱🄾🅁 🄴🅇🄿🅁🄴🅂🅂""")
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-escolha_do_user = int(input('Escolha uma opção: '))
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
 def finalizar_app():
     os.system('cls')
     print('Finalizou app\n')
 
+def ecolher_opcao():
+    escolha_do_user = int(input('Escolha uma opção: '))
 
-
-if escolha_do_user == 1:
-    print('Cadastrar restaurante')
-elif escolha_do_user == 2:
-    print('Listar restaurante')
-elif escolha_do_user == 3:
-    print('Ativar restaurante')
-else:
-    finalizar_app()
+    match escolha_do_user:
+        case 1:
+            print('Cadastrar restaurante')
+        case 2:
+            print('Listar restaurante')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            finalizar_app()
+    
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    ecolher_opcao()
+    
+if __name__ == '__main__':
+    main()
