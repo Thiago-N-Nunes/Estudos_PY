c7cebcc425d8fd19722e765ad801739e16114f24
Thiago Nunes <thiagonesi.cntt@hotmail.com>
2026-02-27 22:22:52 -0300

    Primeiro modulo sobre manipulação de  Strings e Inputs finalizados

--- Curso_Python_Alura/sabor-express/main.py
+++ Curso_Python_Alura/sabor-express/main.py
@@ -1,7 +1,9 @@
-print('Sabor Express')
+print("""🅂🄰🄱🄾🅁 🄴🅇🄿🅁🄴🅂🅂""")
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
-print('4. Sair')
+print('4. Sair\n')
 
+escolha_do_user = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {escolha_do_user}')
