4040de3d2b20979f88193ad4018ade106cfc995f
Thiago Nunes <thiagonesi.cntt@hotmail.com>
2026-02-28 10:00:47 -0300

    Utilização de condicionais

--- Curso_Python_Alura/sabor-express/main.py
+++ Curso_Python_Alura/sabor-express/main.py
@@ -4,6 +4,13 @@ print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
-
 escolha_do_user = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {escolha_do_user}')
+
+if escolha_do_user == 1:
+    print('Cadastrar restaurante')
+elif escolha_do_user == 2:
+    print('Listar restaurante')
+elif escolha_do_user == 3:
+    print('Ativar restaurante')
+else:
+    print('Sair')
